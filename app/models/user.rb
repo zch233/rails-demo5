2471f246fc878a21436b39ddd78a8ea355489d84
zhangchao <529743595@qq.com>
2023-12-10 22:07:17 +0800

    fix: fix user model validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :email
-  # validates_uniqueness_of :email
+  validates_uniqueness_of :email
   validates_presence_of :password_confirmation, on: [:create]
 
-  validates_format_of :email, with: /.+@.+/, if: :email
+  validates_format_of :email, with: /.+@.+/, if: :email, allow_blank: true
   validates_length_of :password, minimum: 6, on: [:create], if: :password
 
   after_create :send_welcome_email
