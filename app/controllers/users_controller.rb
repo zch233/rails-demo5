42d2237d63363049557c0ab0042eff3a15fe04f2
zhangchao <529743595@qq.com>
2023-12-10 19:14:10 +0800

    feat: add user mail unique

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def create
     u = User.create create_params
-    UserMailer.with(user: u).welcome_email.deliver_later
     render_resource u
+    UserMailer.with(user: u).welcome_email.deliver_later if u.errors.empty?
   end
 
   def create_params
