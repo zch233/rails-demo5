7b43c83f5f246beeb0f5ae74a383bd68ef1f2b75
zhangchao <529743595@qq.com>
2023-12-10 22:07:00 +0800

    docs: add users api doc

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem "puma", "~> 5.0"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
+gem 'rspec_api_documentation'
 
 gem 'mail', '~> 2.7'
 
---
+++ spec/acceptance/users_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "Users" do
+  # header "Content-Type", "application/json"
+  post "/users" do
+    parameter :email, '邮箱', type: :string, required: true
+    parameter :password, '密码', type: :string, required: true
+    parameter :password_confirmation, '确认密码', type: :string, required: true
+    example "创建用户" do
+      do_request email: '1@qq.com', password: '123456', password_confirmation: '123456'
+
+      expect(status).to eq 200
+    end
+  end
+end
