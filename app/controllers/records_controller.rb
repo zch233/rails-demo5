3bf295b31e142764669da529df6b9e1c48e43939
zhangchao <529743595@qq.com>
2023-12-23 19:55:05 +0800

    feat: update records controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,3 +1,27 @@
 class RecordsController < ApplicationController
+  def create
+    render_resource Record.create create_params
+  end
 
+  def index
+    render_resources Record.all
+  end
+
+  def show
+    if params[:id]
+      render_resource Record.find_by_id params[:id]
+    end
+  end
+
+  def destroy
+    if params[:id]
+      render_resource Record.destroy params[:id]
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(:amount, :category, :sign_time)
+  end
 end
