a7c0b28fc47f6b4955eb620b4b064a0829f10a9c
zhangchao <529743595@qq.com>
2023-12-23 15:37:33 +0800

    feat: update sesstion validate

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,7 +5,7 @@ class Session
 
   validates_presence_of :email, :password
   validates_format_of :email, with: /.+@.+/, if: :email, allow_blank: true
-  validate :check_email, if: :email
+  validate :check_email, if: Proc.new {|s| s.email.present?}
   validates_length_of :password, minimum: 6, if: :password
   validate :check_password, if: Proc.new {|s| s.email.present? and s.password.present?}
   # validate :check_password, if: lambda { password.present? && email.present? }
