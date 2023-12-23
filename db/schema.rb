caa87ac596d479a6ea8ed6d06c7e968398a01295
zhangchao <529743595@qq.com>
2023-12-23 21:44:13 +0800

    chore: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_23_103132) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_23_131557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_23_103132) do
     t.integer "amount", null: false
     t.datetime "sign_time", null: false
     t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "icon"
+    t.string "name", limit: 20, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
